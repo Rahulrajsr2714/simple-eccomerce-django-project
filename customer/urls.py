844306e7f605126473dc0d6d952601ae21f7c464
Rahulraj S R <rahulsr2714@gmail.com>
2020-09-12 23:50:04 +0530

    ecommerceDjango 1.0

---
+++ customer/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from . import views
+from django.views.generic import RedirectView
+
+
+urlpatterns = [
+    path('', RedirectView.as_view(url='products')),
+    path('registercustomer', views.registercustomer, name='registercustomer'),
+    path('logout', views.logoutcustomer, name='logoutcustomer'),
+    path('logincustomer', views.logincustomer, name='logincustomer'),
+    path('products', views.homepage, name='products'),
+    path('addtocart', views.addproducttocart, name='addtocart'),
+    path('removefromcart', views.removeproductfromcart, name='removefromcart'),
+    path('viewcustomercart', views.viewcustomercart, name='viewcustomercart'),
+    path('removefromcartpage/<int:cart_item_id>',
+         views.removeproductcartpage, name='removeproductcartpage'),
+    path('checkoutcustomer', views.checkoutcustomer, name='checkoutcustomer'),
+    path('markpaymentsuccess', views.markpaymentsuccess, name='markpaymentsuccess'),
+    path('viewproduct/<int:product_id>',
+         views.viewproductdetails, name='viewproductdetails'),
+]
